2f8a0e13ca79f589ee979f7de555abd5450898e1
Richard Linek <richard.linek@unob.cz>
2025-01-12 10:25:55 +0000

    oprava ukolu 7

--- Ukol_7/python/matice.py
+++ Ukol_7/python/matice.py
@@ -22,7 +22,9 @@ def soucet_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[lis
 
 def nasobeni_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[list[int]]:
     """Vynásobí dvě matice, pokud je násobení proveditelné."""
-    if not matice1 or not matice2 or len(matice1[0]) != len(matice2):
+    if not matice1 or not matice2:
+        return []
+    if len(matice1[0]) != len(matice2):
         print("Chyba: Počet sloupců první matice není roven počtu řádků druhé matice.")
         return None
     return [[sum(a * b for a, b in zip(row, col)) for col in zip(*matice2)] for row in matice1]
@@ -30,8 +32,8 @@ def nasobeni_matic(matice1: list[list[int]], matice2: list[list[int]]) -> list[l
 
 def transpozice_matice(matice: list[list[int]]) -> list[list[int]]:
     """Provede transpozici matice."""
-    if not matice or not matice[0]:
-        return [[]]
+    if not matice:
+        return []
     return [list(row) for row in zip(*matice)]
 
 
